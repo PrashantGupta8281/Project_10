7abc3fa13b8467aa8a9ac35ce4ad0dd232d5e5fc
PrashantGupta8281 <guptaprashant2006@gmail.com>
2026-07-19 13:01:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ st.markdown("""
 # 2. SIDEBAR PANEL (Configuration & Data Engine)
 # ==========================================
 with st.sidebar:
-    st.image("https://algatesinsurance.in/wp-content/uploads/2025/04/Understanding-Domiciliary-Hospitalisation-in-Health-Insurance-1024x1024.png", width=100)
+    st.image("https://algatesinsurance.in/wp-content/uploads/2025/04/Understanding-Domiciliary-Hospitalisation-in-Health-Insurance-1024x1024.png", width=100 height=100)
     st.title("Control Panel")
     st.markdown("---")
 
